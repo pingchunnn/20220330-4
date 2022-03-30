fad7e31bdb8d1a81a45c3e6d28e0e67f5848041b
pingchunnn <alice92729@gmail.com>
2022-03-30 17:10:20 +0800

    加入專案檔案。

---
+++ 20220330-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220330_4
+{
+    internal class Program
+    {
+        private static int fib(int n)
+        { 
+            if (n < 2)
+                return 1;
+            else
+                return fib(n-1) + fib(n-2);
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(fib(i));
+            Console.WriteLine(fib(10));
+            Console.ReadLine();
+        }
+    }
+}
